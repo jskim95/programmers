fca516c75cc40d845bce7033476d073aef488f19
jskim95 <js2725@naver.com>
2019-10-31 12:02:48 +0900

    Update 수박수박수박수박수박수?.js

--- lv1/수박수박수박수박수박수?.js
+++ lv1/수박수박수박수박수박수?.js
@@ -1,3 +1,25 @@
+// 수박수박수박수박수박수?
+
+// 문제 설명
+// 길이가 n이고, 수박수박수박수....와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+// 예를들어 n이 4이면 수박수박을 리턴하고 3이라면 수박수를 리턴하면 됩니다.
+
+// 제한 조건
+// n은 길이 10,000이하인 자연수입니다.
+
+// 입출력 예
+// n	return
+// 3	"수박수"
+// 4	"수박수박"
+
+
+
+
+
+
+
+
+
 function solution(n) {
     var a = "수";
     var b = "박";
@@ -5,8 +27,11 @@ function solution(n) {
     for(var i = 0; i<n; i++)
         if(i%2 == 0){
             answer += a;
+            console.log(answer)
         } else{
             answer += b;
+                        console.log(answer)
+
         }
     
     return answer;
