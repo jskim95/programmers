402869bd6f64cec51b7b7b02f50ed995c8feb07d
jskim95 <js2725@naver.com>
2020-06-12 11:59:33 +0900

    Create N개의 최소공배수.js

---
+++ lv2/N개의 최소공배수.js
@@ -0,0 +1,41 @@
+// N개의 최소공배수
+
+// 문제 설명
+// 두 수의 최소공배수(Least Common Multiple)란 입력된 두 수의 배수 중 공통이 되는 가장 작은 숫자를 의미합니다. 예를 들어 2와 7의 최소공배수는 14가 됩니다. 정의를 확장해서, n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 가장 작은 숫자가 됩니다. n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, solution을 완성해 주세요.
+
+// 제한 사항
+// arr은 길이 1이상, 15이하인 배열입니다.
+// arr의 원소는 100 이하인 자연수입니다.
+
+// 입출력 예
+// arr	result
+// [2,6,8,14]	168
+// [1,2,3]	6
+
+
+    // 풀이
+    
+    function solution(arr) {
+    var answer = 0;
+    arr.sort(function (a,b){ return b-a; })
+    
+    function again(max, min) {
+        if(arr.length == 1) {
+            return false
+        }
+        var di = max%min
+        if(di === 0 ) {
+            var save = (arr[0]*arr[1])/min
+            arr.shift()
+            arr.shift()
+            arr.unshift(save)
+            again(arr[0], arr[1])
+        } else {
+            max = min
+            min = di
+            again(max, min)
+        }
+    }
+    again(arr[0], arr[1])
+    return arr[0];
+}
